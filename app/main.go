f9c75f3120eb24a38961b79b6ee2221ce1d2f1b3
SAM <saeedmedra@gmail.com>
2026-02-05 15:03:23 +0200

    Add basic exit/echo/type builtinis

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -14,7 +15,26 @@ func main() {
 			fmt.Fprintln(os.Stderr, "Error reading command:", err)
 			return
 		}
+		if cmd == "exit\n" {
+			return
+		}
+		if strings.HasPrefix(cmd, "echo ") {
+			input := cmd[len("echo ") : len(cmd)-1]
+			fmt.Println(input)
+			continue
+		}
 
+		if strings.HasPrefix(cmd, "type ") {
+			arg := strings.TrimSpace(cmd[len("type "):])
+
+			switch arg {
+			case "exit", "echo", "type":
+				fmt.Println(arg + " is a shell builtin")
+			default:
+				fmt.Println(arg + " not found")
+			}
+			continue
+		}
 		fmt.Println(cmd[:len(cmd)-1] + ": not found")
 	}
 }
